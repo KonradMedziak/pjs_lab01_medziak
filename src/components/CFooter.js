0db11373dfb98e95957961e6404a26afd7bac053
JakubLubera <jakub.lubera@student.pk.edu.pl>
2024-10-01 16:43:04 +0200

    v5.0

---
+++ src/components/CFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function CFooter(){
+    return (
+    <div>
+        <h5>
+            Pogramowanie JavaScript przykładowy serwis React
+        </h5>
+    </div>
+    
+    )
+}
+
+export default CFooter;
