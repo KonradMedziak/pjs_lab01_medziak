b089114b0cd11a73c32b3fa98fb186f27113fe0e
Konrad Medziak <konrad.medziak@student.pk.edu.pl>
2024-10-03 13:44:00 +0200

    v5.6

--- src/components/CContent.js
+++ src/components/CContent.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import './CContents.css';
+import jsonData from '../res/text.json'
+
 class CContents extends Component{
 
     constructor(){
         super()
         this.state={msg: 'Witaj uzytkowniku. Kliknij przycisk aby poznac wiadomosc'}
+        this.txtData=()=>JSON.parse(JSON.stringify(jsonData));
     }
 
     changeMsg(){
@@ -11,12 +15,24 @@ class CContents extends Component{
     }
 
     render(){
-        return (
-            <div>
+        
+            const items=[];
+            for(let i=0;i<this.txtData().count;i++){
+                let value= this.txtData().text[i];
+                items.push(
+                    <div id={'"div${i+1}"'} className="contentsFlex">
+                        <img className="contentsFlexImg" src={'/img/img'+(i+1)+'.png'} alt=""/>
+                        <p className='contentsFlexTxt'>{value}</p>
+                    </div>
+                );
+            }
+        return(
+            <div className='contentsDivClass'>
+                {items}
                 <h1>
                     {this.state.msg}
                 </h1>
-                <button onClick={()=>this.changeMsg()}>Kliknij aby poznac tajemnice</button>
+                <button onClick={this.changeMsg}>Kliknij na przycisk, aby poznać tajemnicę</button>
             </div>
         )
     }
